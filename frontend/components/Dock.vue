aa461f4711cd207829c4bd0b599303b1bec75eeb
Aleksander Grygierczyk <me@aleksander.cc>
2025-06-27 02:36:18 -0700

    Basic project setup + basic whiteboard view

---
+++ frontend/components/Dock.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex fixed bottom-2 left-2 right-2 justify-center">
+    <div class="p-2 bg-neutral-800 rounded-xl text-white flex gap-2">
+      <button
+        class="p-2 flex bg-neutral-600 rounded-xl hover:bg-neutral-500 transition-colors cursor-pointer"
+      >
+        <Icon name="mdi:pen" />
+      </button>
+      <button
+        class="p-2 flex bg-neutral-600 rounded-xl hover:bg-neutral-500 transition-colors cursor-pointer"
+      >
+        <Icon name="mdi:eraser" />
+      </button>
+    </div>
+  </div>
+</template>
